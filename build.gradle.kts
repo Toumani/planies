9b871c1fb881159e60438c769bd02ea2ae429b09
Toumani <toumani49@gmail.com>
2021-10-15 15:26:11 +0000

    fix: change sdk from 11 to 1.8
    TornadoFX encounters several issues with jdk 9+. TornadoFX 2, which is not production ready at time of writing, aims jdk 11+. We will eventually switch to TornadoFX 2 in order to target latest jvm versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.32"
     application
-    id("org.openjfx.javafxplugin") version "0.0.8"
-}
-javafx {
-    version = "11"
-    modules("javafx.controls", "javafx.graphics")
 }
 
 group = "com.planies.client"
@@ -24,16 +19,15 @@ application {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation("no.tornado:tornadofx:$tornadofx_version")
     testImplementation(kotlin("test-junit"))
 }
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
     }
 }
